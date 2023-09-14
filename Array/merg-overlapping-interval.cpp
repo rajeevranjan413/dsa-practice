2054dc753522a1092f5afd85427c5ff2bb2d7161
rajeevranjan413 <rajuranjan413@gmail.com>
2023-09-14 11:21:59 +0530

    corrected naming convention

---
+++ Array/merg-overlapping-interval.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// vector<vector<int>> mergOverLappingIntervals(vector<vector<int>> &intervals)
+// {
+//     sort(intervals.begin(), intervals.end());
+// }
+
+
+
+int main()
+{
+
+    int arr[10]={2,3,4,5,6};
+    int arr1[20]=arr;
+    for(int i=0; i<10; i++){
+        cout<<arr1[i];
+    }
+    // vector<vector<int>> intervels = {{1, 4}, {3, 6}, {7, 8}, {11, 15}};
+    // // intervels = mergOverLappingIntervals(intervels);
+    // vector<vector<int>> second = intervels;
+    // second[0][0] = 10;
+
+    // for (int i = 0; i < intervels.size(); i++)
+    // {
+    //     for (int j = 0; j < intervels[0].size(); j++)
+    //     {
+    //         cout << intervels[i][j] << " ";
+    //     }
+    //     cout << endl;
+        
+    // }
+    // cout<<endl;
+    // for (int i = 0; i < intervels.size(); i++)
+    // {
+    //     for (int j = 0; j < intervels[0].size(); j++)
+    //     {
+    //         cout << second[i][j] << " ";
+    //     }
+    //     cout << endl;
+        
+    // }
+
+    // return 0;
+}
