59e9d8931baf00f928b0e843ae042abdacce7564
rajeevranjan413 <rajuranjan413@gmail.com>
2023-09-14 11:25:40 +0530

    edited

--- Array/merg-overlapping-interval.cpp
+++ Array/merg-overlapping-interval.cpp
@@ -1,22 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 // vector<vector<int>> mergOverLappingIntervals(vector<vector<int>> &intervals)
 // {
 //     sort(intervals.begin(), intervals.end());
 // }
 
-
-
 int main()
 {
 
-    int arr[10]={2,3,4,5,6};
-    int arr1[20]=arr;
-    for(int i=0; i<10; i++){
-        cout<<arr1[i];
-    }
     // vector<vector<int>> intervels = {{1, 4}, {3, 6}, {7, 8}, {11, 15}};
     // // intervels = mergOverLappingIntervals(intervels);
     // vector<vector<int>> second = intervels;
@@ -29,7 +21,7 @@ int main()
     //         cout << intervels[i][j] << " ";
     //     }
     //     cout << endl;
-        
+
     // }
     // cout<<endl;
     // for (int i = 0; i < intervels.size(); i++)
@@ -39,7 +31,7 @@ int main()
     //         cout << second[i][j] << " ";
     //     }
     //     cout << endl;
-        
+
     // }
 
     // return 0;
